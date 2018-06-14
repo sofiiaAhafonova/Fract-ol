a199e3bc15476203e95c6c741de5bd20149188c8
sahafono <amita051197@gmail.com>
2018-06-14 19:17:17 +0300

    legend modified

--- src/map_legend.c
+++ src/map_legend.c
@@ -2,18 +2,14 @@
 
 int 	put_legend(t_env *env)
 {
-	char	*buf;
-
 	if (!env->show_legend)
 		return (1);
 	mlx_string_put(env->mlx_ptr, env->window, 20, 20, 0xFFFFFF, "LEGEND:");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 40, 0xFFFFFF, "X:");
-	buf = ft_itoa(env->mouse_offset_x);
-	mlx_string_put(env->mlx_ptr, env->window, 40, 40, 0xFFFFFF, buf);
-	free(buf);
-	mlx_string_put(env->mlx_ptr, env->window, 20, 60, 0xFFFFFF, "Y:");
-	buf = ft_itoa(env->mouse_offset_y);
-	mlx_string_put(env->mlx_ptr, env->window, 40, 60, 0xFFFFFF, buf);
-	free(buf);
+	mlx_string_put(env->mlx_ptr, env->window, 20, 40, 0xFFFFFF, "Decrease/increase n: 1/2");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 60, 0xFFFFFF, "Zoom in/out: +/-");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 80, 0xFFFFFF, "Hide/show legend: h");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 100, 0xFFFFFF, "move: arrows");
+	if (env->fractal.type == JULIA)
+		mlx_string_put(env->mlx_ptr, env->window, 20, 120, 0xFFFFFF, "begin/start change: space");
 	return (0);
 }
