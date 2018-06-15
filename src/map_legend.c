4e915e6d4e29575862cc4dbeb3f4c2e4dd61bae6
sahafono <amita051197@gmail.com>
2018-06-15 16:53:06 +0300

    norme map legend

--- src/map_legend.c
+++ src/map_legend.c
@@ -1,16 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_legend.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sahafono <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/15 16:51:26 by sahafono          #+#    #+#             */
+/*   Updated: 2018/06/15 16:51:27 by sahafono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/fractol.h"
 
-int 	put_legend(t_env *env)
+int		put_legend(t_env *env)
 {
 	if (!env->show_legend)
 		return (1);
-	mlx_string_put(env->mlx_ptr, env->window, 20, 40, 0xFFFFFF, "CHANGE N(-/+): 1/2");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 60, 0xFFFFFF, "ZOOM IN/OUT: +/-");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 80, 0xFFFFFF, "HIDE/SHOW LEGEND: h");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 100, 0xFFFFFF, "MOVE: ARROWS");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 120, 0xFFFFFF, "CHANGE COLORS: 4");
-	mlx_string_put(env->mlx_ptr, env->window, 20, 140, 0xFFFFFF, "RESET: 0");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 40, 0xFFFFFF,
+		"CHANGE N(-/+): 1/2");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 60, 0xFFFFFF,
+		"ZOOM IN/OUT: +/-");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 80, 0xFFFFFF,
+		"HIDE/SHOW LEGEND: h");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 100, 0xFFFFFF,
+		"MOVE: ARROWS");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 120, 0xFFFFFF,
+		"CHANGE COLORS: 4");
+	mlx_string_put(env->mlx_ptr, env->window, 20, 140, 0xFFFFFF,
+		"RESET: 0");
 	if (env->fractal.type == JULIA)
-		mlx_string_put(env->mlx_ptr, env->window, 20, 160, 0xFFFFFF, "begin/start change: space");
+		mlx_string_put(env->mlx_ptr, env->window, 20, 160,
+			0xFFFFFF, "begin/start change: space");
 	return (0);
 }
