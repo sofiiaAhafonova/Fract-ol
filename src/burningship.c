236dde7c7e9cc346af3d149f9f16ee7f9261e50d
sahafono <amita051197@gmail.com>
2018-06-15 16:10:03 +0300

    norme burningship

--- src/burningship.c
+++ src/burningship.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   burningship.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sahafono <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/15 16:06:41 by sahafono          #+#    #+#             */
+/*   Updated: 2018/06/15 16:06:55 by sahafono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/fractol.h"
 
-
-void	each_iter_burn(t_env *env, int x, int y, t_complex c)
+void			each_iter_burn(t_env *env, int x, int y, t_complex c)
 {
 	int			i;
 	t_complex	z0;
@@ -10,26 +21,24 @@ void	each_iter_burn(t_env *env, int x, int y, t_complex c)
 	i = 0;
 	z0.x = c.x;
 	z0.y = c.y;
-	while (z0.x*z0.x + z0.y * z0.y <= 4 && ++i <= env->fractal.n)
+	while (z0.x * z0.x + z0.y * z0.y <= 4 && ++i <= env->fractal.n)
 	{
 		buf = z0.x * z0.x - z0.y * z0.y + c.x;
-		z0.y = fabs(2 * z0.x * z0.y)+ c.y;
+		z0.y = fabs(2 * z0.x * z0.y) + c.y;
 		z0.x = fabs(buf);
 	}
 	if (i > env->fractal.n)
 		env->image.data[y * SCREEN_WIDTH + x] = 0;
 	else
-		env->image.data[y * SCREEN_WIDTH + x] = i + env->fractal.color_func(i) ;
+		env->image.data[y * SCREEN_WIDTH + x] = i + env->fractal.color_func(i);
 }
 
-
-
-void burningship(t_env *env)
+void			burningship(t_env *env)
 {
 	int			x;
-	int 		y;
+	int			y;
 	t_complex	c;
-	int 		l;
+	int			l;
 
 	x = -1;
 	l = SCREEN_WIDTH < SCREEN_HEIGHT ? SCREEN_WIDTH : SCREEN_HEIGHT;
@@ -38,10 +47,13 @@ void burningship(t_env *env)
 		y = -1;
 		while (++y < SCREEN_HEIGHT)
 		{
-			c.x = 4 * (x + env->offset_x - SCREEN_WIDTH / 2.0) / (l * env->fractal.zoom);
-			c.y = 4 * (y + env->offset_y - SCREEN_HEIGHT / 2.0) / (l * env->fractal.zoom);
+			c.x = 4 * (x + env->offset_x - SCREEN_WIDTH / 2.0) /
+					(l * env->fractal.zoom);
+			c.y = 4 * (y + env->offset_y - SCREEN_HEIGHT / 2.0) /
+					(l * env->fractal.zoom);
 			each_iter_burn(env, x, y, c);
 		}
 	}
-	mlx_put_image_to_window(env->mlx_ptr, env->window, env->image.img_ptr, 0, 0);
+	mlx_put_image_to_window(env->mlx_ptr, env->window,
+		env->image.img_ptr, 0, 0);
 }
