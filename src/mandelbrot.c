99d87d9f09f094226a51d51ab38d5c1e02b5b0be
sahafono <amita051197@gmail.com>
2018-06-15 16:51:03 +0300

    norme mandelbrot

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sahafono <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/15 16:49:40 by sahafono          #+#    #+#             */
+/*   Updated: 2018/06/15 16:49:42 by sahafono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/fractol.h"
 
-
-void	each_iter(t_env *env, int x, int y, t_complex c)
+void			each_iter(t_env *env, int x, int y, t_complex c)
 {
 	int			i;
 	t_complex	z0;
@@ -10,7 +21,7 @@ void	each_iter(t_env *env, int x, int y, t_complex c)
 	i = 0;
 	z0.x = c.x;
 	z0.y = c.y;
-	while (z0.x*z0.x + z0.y * z0.y <= 4 && ++i <= env->fractal.n)
+	while (z0.x * z0.x + z0.y * z0.y <= 4 && ++i <= env->fractal.n)
 	{
 		buf.x = z0.x * z0.x - z0.y * z0.y;
 		buf.y = 2 * z0.x * z0.y;
@@ -20,17 +31,15 @@ void	each_iter(t_env *env, int x, int y, t_complex c)
 	if (i > env->fractal.n)
 		env->image.data[y * SCREEN_WIDTH + x] = 0;
 	else
-		env->image.data[y * SCREEN_WIDTH + x] = i + env->fractal.color_func(i) ;
+		env->image.data[y * SCREEN_WIDTH + x] = i + env->fractal.color_func(i);
 }
 
-
-
-void mandelbrot_set(t_env *env)
+void			mandelbrot_set(t_env *env)
 {
 	int			x;
-	int 		y;
+	int			y;
 	t_complex	c;
-	int 		l;
+	int			l;
 
 	x = -1;
 	l = SCREEN_WIDTH < SCREEN_HEIGHT ? SCREEN_WIDTH : SCREEN_HEIGHT;
@@ -40,11 +49,12 @@ void mandelbrot_set(t_env *env)
 		while (++y < SCREEN_HEIGHT)
 		{
 			c.x = 4 * (x + env->offset_x - SCREEN_WIDTH / 2.0) /
-				  (l * env->fractal.zoom);
+					(l * env->fractal.zoom);
 			c.y = 4 * (y + env->offset_y - SCREEN_HEIGHT / 2.0) /
-				  (l * env->fractal.zoom);
+					(l * env->fractal.zoom);
 			each_iter(env, x, y, c);
 		}
 	}
-	mlx_put_image_to_window(env->mlx_ptr, env->window, env->image.img_ptr, 0, 0);
+	mlx_put_image_to_window(env->mlx_ptr, env->window,
+							env->image.img_ptr, 0, 0);
 }
